3479ae79687c1fb193dbafb9d91ce83e7ffb903f
Jocelyn Ramirez <l24410171@itparral.edu.mx>
2026-05-20 17:23:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtGui import QPalette, QColor
 from pantalla.inicio import InicioWindow
 
 class Snakeverse(QMainWindow):
@@ -7,6 +8,12 @@ class Snakeverse(QMainWindow):
         super().__init__()
         self.setWindowTitle("")
         self.setGeometry(100, 100, 800, 600)
+
+        self.setAutoFillBackground(True)
+        palette = Qpalette()
+        palette.setColor(QPalette.window, QColor(0, 0, 0))
+        self.setPalette(palette)
+        
         self.setCentralWidget(InicioWindow(self))
 
 if __name__ == "__main__":
